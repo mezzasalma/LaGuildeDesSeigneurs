2cec3e3f1bded020dba45ae4cd487193cfe3c0e5
Maëva Mezzasalma <maeva.mezza38@gmail.com>
2020-11-23 15:01:55 +0100

    Resolve Project

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\CharacterRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
  * @ORM\Entity(repositoryClass=CharacterRepository::class)
  * @ORM\Table(name="`characters`")
